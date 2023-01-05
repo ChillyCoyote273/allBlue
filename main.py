53b32b9b91e01d2957be2b4ceff8f0524c5d3abd
May <MayflowerC10@gmail.com>
2023-01-05 13:01:24 -0500

    Cleanup code and search valid moves

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import scipy as sp
 
 def get_moves_from_string(move_string: str):
 	moves = [c for c in move_string if c != ' ']
-	moves = [int(move) for move in moves]
+	try:
+		moves = [int(move) for move in moves]
+	except:
+		return (np.array([]), True)
 	move_array = np.array([0] * 10)
 	for move in moves:
 		move_array[move] = 1
-	return move_array
+	return (move_array, False)
 
 
 def get_moves_from_file():
@@ -24,16 +27,28 @@ def get_flipped_nodes(state: np.ndarray[int]) -> str:
 			readout += f'{i} '
 	return readout
 
+def compute_valid_states(edge_matrix: np.ndarray[int]) -> "list[np.ndarray[int]]":
+	states = []
+	for num in range(2 ** 10):
+		move_array = np.array([0] * 10)
+		for idx in range(10):
+			move_array[idx] = num % 2
+			num >> 1
+		state = edge_matrix.dot(move_array) % 2
+		states.append(state)
+	return states
 
 def main():
 	#                 0  1  2  3  4  5  6  7  8  9
 	# moves = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+	edge_matrix = []
+	with open('edgeMatrix.txt') as file:
+		edge_matrix = file.read()
+		edge_matrix = np.array([[int(elem) for elem in line.split(' ')] for line in edge_matrix.split('\n')])
 	while True:
-		moves = get_moves_from_string(input('Enter a sequence of moves: '))
-		edge_matrix = []
-		with open('edgeMatrix.txt') as file:
-			edge_matrix = file.read()
-			edge_matrix = np.array([[int(elem) for elem in line.split(' ')] for line in edge_matrix.split('\n')])
+		(moves, should_break) = get_moves_from_string(input('Enter a sequence of moves: '))
+		if should_break: 
+			break
 		state = edge_matrix.dot(moves) % 2
 		print(f'Flipped nodes: {get_flipped_nodes(state)}')
 		with open("graph.dot", "w") as file:
@@ -51,5 +66,8 @@ def main():
 					if elem == 1:
 						file.write(f"{i} -> {j} [dir=none]\n")
 			file.write("}\n")
+	for state in compute_valid_states(edge_matrix):
+		if len(get_flipped_nodes(state)) == 4:
+			print(f"Found state with 2 nodes flipped: {get_flipped_nodes(state)}")
 if __name__ == '__main__':
 	main()
