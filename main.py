14d9934bd9f3c0ffcca2157d7bee04adffdc5858
Alex Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2023-01-05 16:11:04 -0500

    can now display graphs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy as sp
+import graphviz
+import os
 
 
 def get_moves_from_string(move_string: str):
@@ -27,47 +29,68 @@ def get_flipped_nodes(state: np.ndarray[int]) -> str:
 			readout += f'{i} '
 	return readout
 
-def compute_valid_states(edge_matrix: np.ndarray[int]) -> "list[np.ndarray[int]]":
-	states = []
+def compute_valid_states(edge_matrix: np.ndarray[int]) -> np.ndarray[int]:
 	for num in range(2 ** 10):
 		move_array = np.array([0] * 10)
 		for idx in range(10):
 			move_array[idx] = num % 2
-			num >> 1
+			num >>= 1
 		state = edge_matrix.dot(move_array) % 2
-		states.append(state)
-	return states
+		yield state, move_array
+
+
+def get_node_position(index: int):
+	INNER_RADIUS = 1
+	OUTER_RADIUS = 2
+	angle = ((index % 5) * 0.4 + 0.5) * np.pi
+	radius = INNER_RADIUS if index > 4 else OUTER_RADIUS
+	x = radius * np.cos(angle)
+	y = radius * np.sin(angle)
+	return f'{-x}, {y}!'
+
+
+def create_graph(edge_matrix, state, name='Petersen Graph'):
+	graph = graphviz.Graph(name, directory='graphs', filename=f'{name}.dot', engine='neato', format='png')
+	for i, vertex in enumerate(state):
+		graph.node(f'{i}', f'{i}', color = 'red' if vertex == 1 else 'blue', pos = get_node_position(i))
+	for i, row in enumerate(edge_matrix):
+		for j, edge in enumerate(row):
+			if i < j and edge == 1:
+				graph.edge(f'{i}', f'{j}', constraint='false')
+	graph.render()
+
+
+def clean_graph_directory():
+	dir_name = 'C:\\Users\\Alex Prichard\\VSCodeProjects\\Python\\allBlue\\graphs\\'
+	directory = os.listdir(dir_name)
+	for file in directory:
+		if file.endswith('.dot'):
+			os.remove(os.path.join(dir_name, file))
+
+
+def rotate_state(state):
+	
+
 
 def main():
-	#                 0  1  2  3  4  5  6  7  8  9
-	# moves = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 	edge_matrix = []
 	with open('edgeMatrix.txt') as file:
 		edge_matrix = file.read()
 		edge_matrix = np.array([[int(elem) for elem in line.split(' ')] for line in edge_matrix.split('\n')])
-	while True:
-		(moves, should_break) = get_moves_from_string(input('Enter a sequence of moves: '))
-		if should_break: 
-			break
-		state = edge_matrix.dot(moves) % 2
-		print(f'Flipped nodes: {get_flipped_nodes(state)}')
-		with open("graph.dot", "w") as file:
-			file.write("digraph G {\n")
-			node_counter = 0
-			for i, node in enumerate(state):
-				if node == 1:
-					file.write(f'{i} [color="red"]\n')
-				else:
-					file.write(f'{i} [color="blue"]\n')
-			for i, row in enumerate(edge_matrix):
-				for j, elem in enumerate(row):
-					if i <= j:
-						continue
-					if elem == 1:
-						file.write(f"{i} -> {j} [dir=none]\n")
-			file.write("}\n")
-	for state in compute_valid_states(edge_matrix):
-		if len(get_flipped_nodes(state)) == 4:
-			print(f"Found state with 2 nodes flipped: {get_flipped_nodes(state)}")
+	
+	four_states = {}
+	for state, moves in compute_valid_states(edge_matrix):
+		state_string = get_flipped_nodes(state)
+		if len(state_string) == 4 * 2:
+			if state_string not in four_states:
+				four_states |= {state_string: (state.copy(), moves.copy())}
+	print(f'Found {len(four_states)} states with 4 blue nodes.')
+	index = 0
+	for state_string, state in four_states.items():
+		create_graph(edge_matrix, state[0], f'graph_{index}')
+		print(f'{index}: {" ".join([str(node) for node in state[1]])}, {state_string}')
+		index += 1
+	clean_graph_directory()
+
 if __name__ == '__main__':
 	main()
